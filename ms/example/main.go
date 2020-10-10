04c3b03e307b0afc0684ffec218d94abe21ca025
lansheng <sheng.lan@imaygou.com>
2020-10-10 11:34:29 +0800

    up

---
+++ ms/example/main.go
@@ -0,0 +1,9 @@
+package main
+
+
+func main() {
+	// config parser, (struct, env, yaml, toml)
+	// init middleware, (support mysql, redis, mongo, es)
+	// config micro service middleware, like trace, monitor， metrics(jaeger, prometheus, grafana, grpc)
+	// start micro service
+}
