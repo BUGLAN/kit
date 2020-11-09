b4bd9d0d1daea9f80df82dcf01af37b3df1c25fb
buglan <1831353087@qq.com>
2020-11-09 22:34:40 +0800

    up

--- config/bootstrap.go
+++ config/bootstrap.go
@@ -3,14 +3,14 @@ package config
 import "github.com/spf13/pflag"
 
 var (
-	Debug = pflag.Bool("debug", false, "debug mode")
+	Debug    = pflag.Bool("debug", false, "debug mode")
 	LogLevel = pflag.String("log_level", "INFO", "log level")
 )
 
 func init() {
 	c := NewKitConfig()
 	pflag.Parse()
-	if err := c.LoadFlag(pflag.CommandLine);  err != nil {
+	if err := c.LoadFlag(pflag.CommandLine); err != nil {
 		c.logger.Panic().Err(err)
 	}
 }
