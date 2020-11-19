98d8316643967bfaeee12a76b99a44b638662045
buglan <1831353087@qq.com>
2020-11-19 22:11:54 +0800

    up

---
+++ jaegerutil/jaeger.go
@@ -0,0 +1,28 @@
+package jaegerutil
+
+import (
+	"github.com/opentracing/opentracing-go"
+	"github.com/pkg/errors"
+	"github.com/uber/jaeger-client-go"
+	"github.com/uber/jaeger-client-go/config"
+	"io"
+)
+
+func InitJaeger(service string) (opentracing.Tracer, io.Closer) {
+	cfg := &config.Configuration{
+		ServiceName: service,
+		Sampler: &config.SamplerConfig{
+			Type:  jaeger.SamplerTypeConst,
+			Param: 1,
+		},
+		Reporter: &config.ReporterConfig{
+			LogSpans:           true,
+			LocalAgentHostPort: "127.0.0.1:6831",
+		},
+	}
+	tracer, closer, err := cfg.NewTracer(config.Logger(jaeger.StdLogger))
+	if err != nil {
+		panic(errors.Wrap(err, "jaeger set config fail"))
+	}
+	return tracer, closer
+}
